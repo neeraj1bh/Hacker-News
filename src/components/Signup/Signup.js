e120466bdaf7ee8e6b914638697a1834e079a9b4
neeraj1bh <neeraj1bh@gmail.com>
2021-01-12 07:47:38 +0530

    Added Signup and Login Pages

---
+++ src/components/Signup/Signup.js
@@ -0,0 +1,78 @@
+import React, { useRef, useState } from "react";
+import { useAuth } from "../../contexts/AuthContext";
+import { Link, useHistory } from "react-router-dom";
+import "./Signup.css";
+import Footer from "../Footer";
+
+export default function Signup() {
+  const emailRef = useRef();
+  const passwordRef = useRef();
+  const passwordConfirmRef = useRef();
+  const { signup } = useAuth();
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
+  const history = useHistory();
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    if (passwordRef.current.value !== passwordConfirmRef.current.value) {
+      return setError("Passwords do not match");
+    }
+
+    try {
+      setError("");
+      setLoading(true);
+      await signup(emailRef.current.value, passwordRef.current.value);
+      history.push("/");
+    } catch {
+      setError("Failed to create an account");
+    }
+
+    setLoading(false);
+  }
+
+  return (
+    <div className="mainC">
+      <div className="containerSign">
+        <div className="c12">
+          <div className="controls">
+            <form className="signup" onSubmit={handleSubmit}>
+              <h2 className="signup1">Sign Up</h2>
+              {error && <div>{error}</div>}
+              <input
+                className="username"
+                type="email"
+                ref={emailRef}
+                placeholder="Email*"
+                required
+              />
+              <input
+                className="username"
+                type="password"
+                ref={passwordRef}
+                placeholder="Password*"
+                required
+              />
+              <input
+                className="username"
+                type="password"
+                ref={passwordConfirmRef}
+                placeholder="Confirm Password*"
+                required
+              />
+              <button className="btn" disabled={loading} type="submit">
+                Sign Up
+              </button>
+            </form>
+            <div className="signup2">
+              Already have an account? <Link to="/login">Log In</Link>
+            </div>
+          </div>
+          {error && <div className="error">{error}</div>}
+        </div>
+      </div>
+      <Footer className="footer" />
+    </div>
+  );
+}
